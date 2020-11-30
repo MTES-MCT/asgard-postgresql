ed04fbd5f30b5f00dee3e48703422d3c61bc5e41
alhyss <55992003+alhyss@users.noreply.github.com>
2020-11-30 11:26:15 +0100

    Update to version 1.2.2

---
+++ asgard--1.2.1--1.2.2.sql
@@ -0,0 +1,68 @@
+\echo Use "CREATE EXTENSION asgard" to load this file. \quit
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- ASGARD - Système de gestion des droits pour PostgreSQL, version 1.2.2
+-- > Script de mise à jour depuis la version 1.2.1.
+--
+-- Copyright République Française, 2020.
+-- Secrétariat général du Ministère de la transition écologique, du
+-- Ministère de la cohésion des territoires et des relations avec les
+-- collectivités territoriales et du Ministère de la Mer.
+-- Service du numérique.
+--
+-- contributeurs : Alain Ferraton (SNUM/MSP/DS/GSG) et Leslie Lemaire
+-- (SNUM/UNI/DRC).
+-- 
+-- mél : drc.uni.snum.sg@developpement-durable.gouv.fr
+-- 
+--
+-- Ce logiciel est un programme informatique complémentaire au système de
+-- gestion de base de données PosgreSQL ("https://www.postgresql.org/"). Il
+-- met à disposition un cadre méthodologique et des outils pour la gestion
+-- des droits sur les serveurs PostgreSQL.
+--
+-- Ce logiciel est régi par la licence CeCILL-B soumise au droit français
+-- et respectant les principes de diffusion des logiciels libres. Vous
+-- pouvez utiliser, modifier et/ou redistribuer ce programme sous les
+-- conditions de la licence CeCILL-B telle que diffusée par le CEA, le
+-- CNRS et l'INRIA sur le site "http://www.cecill.info".
+-- Lien SPDX : "https://spdx.org/licenses/CECILL-B.html".
+--
+-- En contrepartie de l'accessibilité au code source et des droits de copie,
+-- de modification et de redistribution accordés par cette licence, il n'est
+-- offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+-- seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+-- titulaire des droits patrimoniaux et les concédants successifs.
+--
+-- A cet égard  l'attention de l'utilisateur est attirée sur les risques
+-- associés au chargement,  à l'utilisation,  à la modification et/ou au
+-- développement et à la reproduction du logiciel par l'utilisateur étant 
+-- donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+-- manipuler et qui le réserve donc à des développeurs et des professionnels
+-- avertis possédant  des  connaissances  informatiques approfondies.  Les
+-- utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+-- logiciel à leurs besoins dans des conditions permettant d'assurer la
+-- sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+-- à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+--
+-- Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+-- pris connaissance de la licence CeCILL-B, et que vous en avez accepté
+-- les termes.
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- Cette extension ne peut être installée que par un super-utilisateur
+-- (création de déclencheurs sur évènement).
+--
+-- Elle n'est pas compatible avec les versions 9.4 ou antérieures de
+-- PostgreSQL.
+--
+-- Schémas contenant les objets : z_asgard et z_asgard_admin.
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- Ce script ne contient aucune commande, la version 1.2.2 ayant pour
+-- objet la correction d'une anomalie dans le script d'installation.
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
