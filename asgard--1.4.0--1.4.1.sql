1d96e30ba4386dc3493e98d7a7176881a37b5c47
alhyss <55992003+alhyss@users.noreply.github.com>
2023-11-02 09:31:17 +0100

    refactor: Initialisation v1.4.1
    Copie du script de la v1.4.0 avec mise à jour de la description.

---
+++ asgard--1.4.0--1.4.1.sql
@@ -0,0 +1,88 @@
+\echo Use "ALTER EXTENSION plume_pg UPDATE TO '1.4.0'" to load this file. \quit
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- ASGARD - Système de gestion des droits pour PostgreSQL, version 1.4.1
+-- > Script de mise à jour depuis la version 1.4.0
+--
+-- Copyright République Française, 2020-2023.
+-- Secrétariat général du Ministère de la Transition écologique et
+-- de la Cohésion des territoires, du Ministère de la Transition
+-- énergétique et du Secrétariat d'Etat à la Mer.
+-- Direction du numérique.
+--
+-- contributrice pour cette version : Leslie Lemaire (DNUM/UNI/DRC).
+-- 
+-- mél : drc.uni.dnum.sg@developpement-durable.gouv.fr
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- Note de version :
+-- https://snum.scenari-community.org/Asgard/Documentation/#SEC_1-4-1
+-- 
+-- Documentation :
+-- https://snum.scenari-community.org/Asgard/Documentation
+-- 
+-- GitHub :
+-- https://github.com/MTES-MCT/asgard-postgresql
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- Ce logiciel est un programme informatique complémentaire au système de
+-- gestion de base de données PosgreSQL ("https://www.postgresql.org/"). Il
+-- met à disposition un cadre méthodologique et des outils pour la gestion
+-- des droits sur les serveurs PostgreSQL.
+--
+-- Ce logiciel est régi par la licence CeCILL-B soumise au droit français
+-- et respectant les principes de diffusion des logiciels libres. Vous
+-- pouvez utiliser, modifier et/ou redistribuer ce programme sous les
+-- conditions de la licence CeCILL-B telle que diffusée par le CEA, le
+-- CNRS et l'INRIA sur le site "http://www.cecill.info".
+-- Lien SPDX : "https://spdx.org/licenses/CECILL-B.html".
+--
+-- En contrepartie de l'accessibilité au code source et des droits de copie,
+-- de modification et de redistribution accordés par cette licence, il n'est
+-- offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+-- seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+-- titulaire des droits patrimoniaux et les concédants successifs.
+--
+-- A cet égard l'attention de l'utilisateur est attirée sur les risques
+-- associés au chargement,  à l'utilisation,  à la modification et/ou au
+-- développement et à la reproduction du logiciel par l'utilisateur étant 
+-- donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+-- manipuler et qui le réserve donc à des développeurs et des professionnels
+-- avertis possédant  des  connaissances  informatiques approfondies.  Les
+-- utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+-- logiciel à leurs besoins dans des conditions permettant d'assurer la
+-- sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+-- à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+--
+-- Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+-- pris connaissance de la licence CeCILL-B, et que vous en avez accepté
+-- les termes.
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+--
+-- Cette extension ne peut être installée que par un super-utilisateur
+-- (création de déclencheurs sur évènement).
+--
+-- Elle n'est pas compatible avec les versions 9.4 ou antérieures de
+-- PostgreSQL.
+--
+-- schémas contenant les objets : z_asgard et z_asgard_admin.
+--
+-- objets créés par le script :
+-- Néant.
+--
+-- objets modifiés par le script (parfois seulement leur descriptif) :
+-- Néant.
+--
+-- objets supprimés par le script :
+-- Néant.
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+-- MOT DE PASSE DE CONTRÔLE : 'x7-A;#rzo'
+
+
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
